819bd09dbf503decfe53994a308cded0c58bf584
Abinash Pradhan <>
2024-05-16 12:26:19 +0530

    enhanced landing and sign in
    enhanced landing and sign in

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,115 +1,159 @@
-'use client'
+"use client";
 import signIn from "@/firebase/auth/signin";
-import { Box, Button, Card, CardContent, FormControl, FormHelperText, Grid, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  CardContent,
+  FormControl,
+  Grid,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
 import { useFormik } from "formik";
 import { useRouter } from "next/navigation";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 
 import React, { useState } from "react";
 import { z } from "zod";
 
 const SignInPage = () => {
-    const [errors,setErrors]= useState({})
-    const router = useRouter()
-    const validationSchema = (values: any) => {
-        const LoggInUserForm = z.object({
-            email: z.string().email("Invalid email address"),
-            password: z.string().min(6, "Password length atleast 6 character long")
-        });
-
-        try {
-            LoggInUserForm.parse(values);
-        } catch (error) {
-           
-            if (error instanceof z.ZodError) {
-                console.log("ERR: ", error.formErrors.fieldErrors)
-                setErrors({...error.formErrors.fieldErrors});
-                return error.formErrors.fieldErrors
-            }
-        }
-
-    }
-
-    const handleLogin = async (values: { email: string, password: string }) => {
-
-        const { result, error } = await signIn(values.email, values.password);
-
-        if (error) {
-            return console.log(error)
-        }
-        router.push('/admin/dashboard');
-    }
-
-
-    const loginFormik = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-        },
-        validate: validationSchema,
-        onSubmit: (values: any) => {
-            // console.log("Values:", values)
-            handleLogin({...values});
-        },
+  const [errors, setErrors] = useState({});
+  const router = useRouter();
+  const validationSchema = (values: any) => {
+    const LoggInUserForm = z.object({
+      email: z.string().email("Invalid email address"),
+      password: z.string().min(6, "Password length atleast 6 character long"),
     });
 
+    try {
+      LoggInUserForm.parse(values);
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        console.log("ERR: ", error.formErrors.fieldErrors);
+        setErrors({ ...error.formErrors.fieldErrors });
+        return error.formErrors.fieldErrors;
+      }
+    }
+  };
 
+  const handleLogin = async (values: { email: string; password: string }) => {
+    const { result, error } = await signIn(values.email, values.password);
 
-    return (
-        <>
-            <Grid container sx={{ height: "100%" }} spacing={2}>
-                <Grid item md={6} sx={{ display: { sm: 'none', xs: 'none', md: 'block' }, backgroundColor: "#1976d2", p: 2 }}>
-                    <Typography variant="h5" component="h2" sx={{ color: "white" }}>
-                        Little Flower Public School
-                    </Typography>
-                </Grid>
-                <Grid item md={6} sm={12} xs={12} >
-                    <Box sx={{ height: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
-                        <Card variant="outlined" sx={{ width: "70%", boxShadow: 2 }} >
-                            <CardContent>
-                                <Box sx={{ textAlign: "center", mt: 1, mb: 5 }}>
-                                    <img src="https://firebasestorage.googleapis.com/v0/b/littleflowerpublicschool-3c090.appspot.com/o/lfps_logo.png?alt=media&token=c7028f17-67a6-4069-b347-f7036f99d7a3"
-                                        alt="LFPS Logo" />
-                                </Box>
-                                <form onSubmit={loginFormik.handleSubmit}>
-                                    <Stack direction="column" spacing={2}>
-                                        <FormControl>
-                                            <TextField
-                                                id="email"
-                                                name="email"
-                                                label="Email Id"
-                                                variant="outlined"
-                                                value={loginFormik.values.email}
-                                                onChange={loginFormik.handleChange}
-                                                error={loginFormik.touched.email && Boolean(loginFormik.errors.email)}
-                                                helperText={loginFormik.errors.email && loginFormik.touched.email && String(loginFormik.errors.email)}
-                                            />
-                                        </FormControl>
-                                        <FormControl>
-                                            <TextField
-                                                id="password"
-                                                name="password"
-                                                label="Password"
-                                                variant="outlined"
-                                                type="password"
-                                                value={loginFormik.values.password}
-                                                onChange={loginFormik.handleChange}
-                                                error={loginFormik.touched.password && Boolean(loginFormik.errors.password)}
-                                                helperText={loginFormik.errors.password && loginFormik.touched.password && String(loginFormik.errors.password)}
-                                            />
-                                            
-                                        </FormControl>
+    if (error) {
+      return console.log(error);
+    }
+    router.push("/admin/dashboard");
+  };
 
-                                        <Button variant="contained" type="submit">SIGN IN</Button>
+  const loginFormik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validate: validationSchema,
+    onSubmit: (values: any) => {
+        
+      handleLogin({ ...values });
+    },
+  });
 
-                                    </Stack>
-                                </form>
-                            </CardContent>
-                        </Card>
-                    </Box>
-                </Grid>
-            </Grid>
-        </>
-    )
+  return (
+    <Grid container sx={{ height: "100%" }} spacing={2}>
+      <Grid
+        item
+        md={6}
+        xs={12}
+        sx={{
+          backgroundColor: "#1976d2",
+          p: 2,
+          height: "100%",
+          display: "flex",
+
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ textAlign: "center" }}>
+          <AccountBalanceIcon sx={{ color: "white", fontSize: "100px" }} />
+          <Typography
+            sx={{ color: "white", textTransform: "italic", fontSize: "16px" }}
+          >
+            Little Flower Public Shools
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item md={6} sm={12} xs={12}>
+        <Box
+          sx={{
+            height: "100%",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Card variant="outlined" sx={{ width: "70%", boxShadow: 2 }}>
+            <CardContent>
+              <Box sx={{ textAlign: "center", mt: 1, mb: 5 }}>
+                <img
+                  src="https://firebasestorage.googleapis.com/v0/b/littleflowerpublicschool-3c090.appspot.com/o/lfps_logo.png?alt=media&token=c7028f17-67a6-4069-b347-f7036f99d7a3"
+                  alt="LFPS Logo"
+                />
+              </Box>
+              <form onSubmit={loginFormik.handleSubmit}>
+                <Stack direction="column" spacing={2}>
+                  <FormControl>
+                    <TextField
+                      id="email"
+                      name="email"
+                      label="Email Id"
+                      variant="outlined"
+                      value={loginFormik.values.email}
+                      onChange={loginFormik.handleChange}
+                      error={
+                        loginFormik.touched.email &&
+                        Boolean(loginFormik.errors.email)
+                      }
+                      helperText={
+                        loginFormik.errors.email &&
+                        loginFormik.touched.email &&
+                        String(loginFormik.errors.email)
+                      }
+                    />
+                  </FormControl>
+                  <FormControl>
+                    <TextField
+                      id="password"
+                      name="password"
+                      label="Password"
+                      variant="outlined"
+                      type="password"
+                      value={loginFormik.values.password}
+                      onChange={loginFormik.handleChange}
+                      error={
+                        loginFormik.touched.password &&
+                        Boolean(loginFormik.errors.password)
+                      }
+                      helperText={
+                        loginFormik.errors.password &&
+                        loginFormik.touched.password &&
+                        String(loginFormik.errors.password)
+                      }
+                    />
+                  </FormControl>
+
+                  <Button variant="contained" type="submit">
+                    SIGN IN
+                  </Button>
+                </Stack>
+              </form>
+            </CardContent>
+          </Card>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default SignInPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,52 @@
 "use client";
-import { useAuthContext } from "@/context/AuthContext";
-import { useEffect } from "react";
-import { useRouter } from 'next/navigation'
-
+import { Box, Button, Grid, Typography } from "@mui/material";
+import AccountBalanceIcon from "@mui/icons-material/AccountBalance";
 export default function Page() {
-  const authContext = useAuthContext();
-  const {replace} = useRouter();
+  return (
+    <Grid container sx={{ height: "100%" }} spacing={2}>
+      <Grid
+        item
+        xs={6}
+        sx={{
+          backgroundColor: "#1976d2",
+          height: "100%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Box sx={{ textAlign: "center" }}>
+          <AccountBalanceIcon sx={{ color: "white", fontSize: "100px" }} />
+          <Typography
+            sx={{ color: "white", textTransform: "italic", fontSize: "16px" }}
+          >
+            Little Flower Public Shools
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item  xs={6}>
+        <Box
+          sx={{
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <Box>
+            <Typography color="primary" variant="body2">
+              Ready to embark on your educational journey?
+            </Typography>
+            <Typography color="primary" variant="body2" sx={{ mb: 3 }}>
+              Click below to access your account and unlock the full potential
+              of our platform.
+            </Typography>
 
-  useEffect(() => {
-    if (authContext) {
-        replace("/admin/dashboard")
-    } else {
-        replace("/signin")
-    }
-  }, []);
-
-  return <></>;
+            <Button variant="contained" href="/signin">
+              Log in
+            </Button>
+          </Box>
+        </Box>
+      </Grid>
+    </Grid>
+  );
 }
